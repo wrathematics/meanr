a64a563c7e85f564e59e5ccd6257e10af2fe4418
Drew Schmidt <wrathematics@gmail.com>
2016-12-04 14:11:40 -0500

    use omp; some mem tweaks

--- src/score.c
+++ src/score.c
@@ -2,9 +2,12 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <R_ext/Utils.h>
 #include "RNACI.h"
+
+#include "safeomp.h"
 
 #include "gperf/poshash.h"
 #include "gperf/neghash.h"
@@ -25,7 +28,7 @@ static inline bool is_neg_sentiment(const char *word, const int wordlen)
   return in_neg_set(word, wordlen) != NULL;
 }
 
-static inline int get_sentiment_score(const char *word, const int wordlen)
+static inline int8_t get_sentiment_score(const char *word, const int wordlen)
 {
   if (is_pos_sentiment(word, wordlen))
     return 1;
@@ -49,20 +52,24 @@ static bool check_interrupt()
 
 
 
-static inline size_t max_strlen(SEXP s_)
+static inline size_t max_strlen(SEXP s_, const int len)
 {
-  size_t max = 0;
-  for (int i=0; i<LENGTH(s_); i++)
+  size_t maxlen = 0;
+  
+  #pragma omp parallel for simd schedule(static,1024) if(len>OMP_MIN_SIZE) reduction(max:maxlen)
+  for (int i=0; i<len; i++)
   {
     char *s = CHARPT(s_, i);
     size_t tmp = strlen(s);
-    if (tmp > max)
-      max = tmp;
+    if (tmp > maxlen)
+      maxlen = tmp;
   }
   
-  return max;
+  return maxlen;
 }
 
+
+int omp_get_thread_num(void);
 
 
 SEXP R_score(SEXP s_)
@@ -70,82 +77,100 @@ SEXP R_score(SEXP s_)
   SEXP ret, ret_names;
   SEXP positive, negative, scores, nwords;
   const int len = LENGTH(s_);
-  size_t slen;
-  char *s;
   
   if (TYPEOF(s_) != STRSXP)
     error("input must be a vector of strings");
   
   newRvec(positive, len, "int");
   newRvec(negative, len, "int");
-  newRvec(scores, len, "dbl");
+  newRvec(scores, len, "int");
   newRvec(nwords, len, "int");
   
-  slen = max_strlen(s_);
-  s = malloc(slen * sizeof(*s));
-  CHECKMALLOC(s);
+  const size_t slen = max_strlen(s_, len);
   
+  int8_t check = 0;
   
-  for (int i=0; i<len; i++)
+  #pragma omp parallel shared(check) if(len>OMP_MIN_SIZE)
   {
-    if (i%512 == 0)
+    char *s = NULL;
+    // NOTE uncomment to simulate oom failure
+    // if (omp_get_thread_num() != 1)
+    s = malloc(slen * sizeof(*s));
+    
+    #pragma omp atomic update
+    check += (s == NULL);
+    
+    
+    if (!check)
     {
-      if (check_interrupt())
+      #pragma omp for
+      for (int i=0; i<len; i++)
       {
-        free(s);
-        return R_NilValue;
+        // TODO
+        // if (i%512 == 0)
+        // {
+        //   if (check_interrupt())
+        //   {
+        //     free(s);
+        //     return R_NilValue;
+        //   }
+        // }
+        
+        char *in = CHARPT(s_, i);
+        size_t inlen = strlen(in) + 1;
+        
+        memcpy(s, in, inlen*sizeof(*s));
+        
+        SAFE_SIMD
+        for (int j=0; j<inlen; j++)
+        {
+          if (ispunct(s[j]))
+          s[j] = ' ';
+          
+          s[j] = tolower(s[j]);
+        }
+        
+        
+        int *const pos = INTEGER(positive) + i;
+        int *const neg = INTEGER(negative) + i;
+        int *const sc = INTEGER(scores) + i;
+        int *const nw = INTEGER(nwords) + i;
+        *pos = *neg = *sc = *nw = 0;
+        
+        uint32_t start = 0;
+        uint32_t end;
+        
+        
+        for (uint32_t j=0; j<inlen; j++)
+        {
+          if (isspace(s[j]) || s[j] == '\0')
+          {
+            (*nw)++;
+            end = j;
+            s[end] = '\0'; // for gperf
+            
+            int8_t score = get_sentiment_score(s+start, end-start);
+            *(sc) += score;
+            if (score > 0)
+            (*pos)++;
+            else if (score < 0)
+            (*neg)++;
+            
+            j++;
+            while (isspace(s[j]))
+            j++;
+            
+            start = j;
+          }
+        }
       }
     }
     
-    char *in = CHARPT(s_, i);
-    size_t inlen = strlen(in) + 1;
-    
-    memcpy(s, in, inlen*sizeof(*s));
-    
-    for (int j=0; j<inlen; j++)
-    {
-      if (ispunct(s[j]))
-        s[j] = ' ';
-      
-      s[j] = tolower(s[j]);
-    }
-    
-    
-    int *pos = INTEGER(positive) + i;
-    int *neg = INTEGER(negative) + i;
-    double *sc = REAL(scores) + i;
-    int *nw = INTEGER(nwords) + i;
-    *pos = *neg = *sc = *nw = 0;
-    
-    int start = 0;
-    int end;
-    
-    
-    for (int j=0; j<inlen; j++)
-    {
-      if (isspace(s[j]) || s[j] == '\0')
-      {
-        (*nw)++;
-        end = j;
-        s[end] = '\0'; // for gperf
-        
-        int score = get_sentiment_score(s+start, end-start);
-        *(sc) += score;
-        if (score > 0)
-          (*pos)++;
-        else if (score < 0)
-          (*neg)++;
-        
-        j++;
-        while (isspace(s[j]))
-          j++;
-        
-        start = j;
-      }
-    }
+    free(s);
   }
   
-  free(s);
+  if (check)
+    THROW_MEMERR();
   
   ret_names = make_list_names(4, "positive", "negative", "score", "wc");
   ret = make_dataframe(RNULL, ret_names, 4, positive, negative, scores, nwords);
