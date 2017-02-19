7435f4becb424a4b9ae2639b7dd6cb66b75e0ae7
wrathematics <wrathematics@gmail.com>
2017-02-19 18:29:41 -0500

    move hashtable stuff around

--- src/gperf/makehash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gperf --includes --readonly-tables --pic --struct-type --slot-name=word words.txt > sentiment.h
--- src/gperf/makewords.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# positive 1, negative -1
-echo "struct sentwords{ char *word; int score; };" > words.txt
-echo "%%" >> words.txt
-
-sed 's/.*/&, 1/' positive.txt >> words.txt
-sed 's/.*/&, -1/' negative.txt >> words.txt
