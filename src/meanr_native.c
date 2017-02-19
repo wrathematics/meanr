f1db4980634b5b64956bc61b47f1231dbd4d5869
wrathematics <wrathematics@gmail.com>
2017-02-19 17:23:44 -0500

    native routines

---
+++ src/meanr_native.c
@@ -0,0 +1,18 @@
+/* Automatically generated. Do not edit by hand. */
+  
+  #include <R.h>
+  #include <Rinternals.h>
+  #include <R_ext/Rdynload.h>
+  #include <stdlib.h>
+
+extern SEXP R_score(SEXP s_);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"R_score", (DL_FUNC) &R_score, 1},
+  {NULL, NULL, 0}
+};
+void R_init_meanr(DllInfo *dll)
+                       {
+                       R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+                       R_useDynamicSymbols(dll, FALSE);
+                       }
