82640b52c10472c60a4a4b747dc4dcb245408262
wrathematics <wrathematics@gmail.com>
2019-03-14 21:36:45 -0400

    tweak description

--- R/meanr-package.r
+++ R/meanr-package.r
@@ -1,14 +1,13 @@
 #' meanr: Sentiment Analysis Scorer
 #' 
-#' Sentiment analysis is a popular technique in text mining. Roughly
-#' speaking, the technique is an attempt to determine the overall emotional
-#' attitude of a piece of text (i.e., positive or negative). We provide a new
+#' Sentiment analysis is a popular technique in text mining that
+#' attempts to determine the emotional state of some text. We provide a new
 #' implementation of a common method for computing sentiment, whereby words are
-#' scored as positive or negative according to a "dictionary", and then an
-#' sum of those scores for the document is produced. We use the 'Hu' and 'Liu'
-#' sentiment dictionary for determining sentiment. The scoring function is
-#' 'vectorized' by document, and scores for multiple documents are computed in
-#' parallel via 'OpenMP'.
+#' scored as positive or negative according to a dictionary lookup. Then the
+#' sum of those scores is returned for the document. We use the Hu and Liu
+#' sentiment dictionary (Hu and Liu, 2004) <doi:10.1145/1014052.1014073>
+#' for determining sentiment. The scoring function is vectorized by document,
+#' and scores for multiple documents are computed in parallel via OpenMP.
 #' 
 #' @name meanr-package
 #' @docType package
